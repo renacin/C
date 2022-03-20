fac33c79d12ac6787f368bbe6a3659b9b90667e1
Renacin <renacinm@gmail.com>
2022-03-20 15:36:24 -0400

    Baby steps

---
+++ Basics/Tutorial_1/main.c
@@ -0,0 +1,30 @@
+// Name:                                            Renacin Matadeen
+// Date:                                               03/20/2022
+// Title                                        Learning C/C++: Tutorial #1
+//
+// ----------------------------------------------------------------------------------------------------------------------
+/*
+
+Notes:
+    +
+
+
+Resources:
+    +
+
+*/
+// ----------------------------------------------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+
+// The main method is a way to store all of our code that must be excecuted on run time
+int main()
+{
+    // You can make comments with double forward slashes
+    printf("Hello world!\n");
+    return 0;
+}
