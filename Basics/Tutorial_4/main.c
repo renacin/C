51567ed1ddf28522c8dc86df2e1d399d9329f1ae
Renacin <renacinm@gmail.com>
2022-03-27 14:19:08 -0400

    Look into memory management in C

--- Basics/Tutorial_4/main.c
+++ Basics/Tutorial_4/main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 // ----------------------------------------------------------------------------------------------------------------------
 
 // Define Custom Function
@@ -47,16 +48,56 @@ int main()
 {
 
     // Define Needed Variables
+    bool exit_cond = true;
+    char cont_val[1];
     float num_1, num_2;
 
-    // Main Code
+    // Greet User
     printf("Num Comparison Bot");
-    printf("\nNumber 1: ");
-    scanf("%f", &num_1);
-    printf("Number 2: ");
-    scanf("%f", &num_2);
+    printf("\nThis Application Will Indicate Which Number Is Bigger");
 
-    which_max_num(num_1, num_2);
+    // Entry Point For Main Loop Of Code
+    while (exit_cond) {
+
+        // Ask User For Input
+        printf("\n\nNumber 1: ");
+        scanf("%f", &num_1);
+        printf("Number 2: ");
+        scanf("%f", &num_2);
+
+        // Compare Numbers
+        which_max_num(num_1, num_2);
+
+        // Ask User If They Would Like To Continue
+        printf("\n\nContinue [Y/N]: ");
+        scanf("%s", &cont_val);
+
+        if (strcmp(cont_val, "Y") == 0){
+            printf("\n Cleaning Values");
+        }
+
+        else if (strcmp(cont_val, "N") == 0) {
+            exit_cond = false;
+        }
+
+        else {
+            printf("\nInvalid Response, restarting app");
+            exit_cond = false;
+
+        }
+    }
+
+    // Thank User For Using Script
+    printf("\nThank You For Using This Script!");
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
@@ -65,15 +106,25 @@ int main()
 
 /*
 Notes:
-    Basic Functions:
-        -
+    Basics Of Loops:
+        - You may encounter situations, when a block of code needs to be executed several number of times
+            In general, statements are executed sequentially: The first statement in a function is executed
+            first, followed by the second, and so on
+
+        - Programming languages provide various control structures that allow for more complicated execution paths
+
+        - There are many types of loops in C
+            + For Loops
+            + While Loops
+            + Do/While Loops
 
 
-    General Syntax Of A Function:
+    General Syntax Of While Loops:
         -
 
 
 Resources:
+    + https://www.guru99.com/c-loop-statement.html
     + https://www.tutorialspoint.com/cprogramming/c_functions.htm
     + https://www.section.io/engineering-education/history-of-c-programming-language/
     + https://www.youtube.com/watch?v=87SH2Cn0s9A
